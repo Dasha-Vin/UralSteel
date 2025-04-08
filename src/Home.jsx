9c8d0223028b258d1ac861828f3fdf41496e43ac
Даша <dashavinaeva98gmail.com@mail.ru>
2025-04-08 11:14:53 +0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,10 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import Header from './components/Header/Header';
 import MainContent from './components/MainContent/MainContent';
+import Programs from './components/Programs/Programs';
+import AboutUs from './components/AboutUs/AboutUs';
+import Contacts from './components/Contacts/Contacts';
 import ChatBot from './components/ChatBot/ChatBot';
-
-const Programs = React.lazy(() => import('./components/Programs/Programs'));
-const AboutUs = React.lazy(() => import('./components/AboutUs/AboutUs'));
-const Contacts = React.lazy(() => import('./components/Contacts/Contacts'));
 
 const Home = ({ onLoginClick }) => {
     return (
@@ -13,18 +12,11 @@ const Home = ({ onLoginClick }) => {
             <ChatBot />
             <Header onLoginClick={onLoginClick}/>
             <MainContent />
-            
-            <Suspense fallback={<div>Загрузка программ...</div>}>
-                <Programs />
-            </Suspense>
-            
-            <Suspense fallback={<div>Загрузка информации о нас...</div>}>
-                <AboutUs />
-            </Suspense>
-            
-            <Suspense fallback={<div>Загрузка контактов...</div>}>
-                <Contacts />
-            </Suspense>
+            <Programs />
+            <AboutUs />
+            <Contacts />
         </main>
     );
 }
+
+export default Home;
